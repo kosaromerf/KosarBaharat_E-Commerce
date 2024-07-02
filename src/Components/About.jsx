ba47a672cea9b0618cb28272327126349fc34b82
kosaromerf <kosaromerf@gmail.com>
2024-07-02 19:18:07 +0300

    Add: Responsive About Page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,32 +4,42 @@ import background from "../assets/background.jpg";
 import Header from "./Header";
 const About = () => {
   return (
-    <div>
-      <header className={styles.header}>
-        <Header></Header>
-      </header>
+    <main>
+      <Header></Header>
       <div className={styles.page}>
         <div className={styles.container}>
-          <h1>Lorem, ipsum dolor.</h1>
-          <p>
-            <img src={background} alt="background" />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non amet
-            nihil beatae quod ullam ad, tempore ipsum corporis aspernatur
-            dolores? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dolorem fugiat dolores perferendis ducimus eaque. Quasi atque
-            assumenda facilis consequuntur iure illum ipsum illo tempore, ipsam
-            accusamus sapiente dicta magni sunt! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Veritatis architecto laudantium
-            quisquam officiis esse tempora debitis repudiandae eum, vitae at, ad
-            reiciendis aperiam? Odio, nam beatae id, quo exercitationem, ut
-            maiores iste quidem corrupti natus est asperiores labore deleniti a.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non amet
-            nihil beatae quod ullam ad, tempore ipsum corporis aspernatur
-            dolores? Lorem ipsum dolor sit amet
+          <h1 className={styles.header}>About Us</h1>
+          <p className={styles.text}>
+            <img
+              className={styles.image}
+              src={background}
+              alt="image of some spices"
+              width={50}
+              height={50}
+            />
+            <span>Kosar Baharat</span> is a family-owned business, established
+            in <span>1997</span>. For <span>over 25 years</span>, we've been
+            bringing the freshest, most flavorful spices and herbs to our
+            community. The owner, Aziz Kosar, has a deep passion for
+            high-quality ingredients. We believe that everyday cooking deserves
+            the best, and that's why we source our spices and herbs with care.
+            You won't find any fillers or artificial ingredients here just pure,
+            delicious flavor.
+          </p>
+          <p className={styles.text}>
+            Reliability is our top priority. We understand the importance of
+            having a store you can trust, and we're committed to providing
+            exceptional service and consistent quality, every time.
+          </p>
+          <p className={styles.text}>
+            Explore our wide selection of all your favorite spices and herbs,
+            and discover the difference fresh ingredients can make in your
+            cooking. We're here to help you create delicious meals for your
+            family and friends.
           </p>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
